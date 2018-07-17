a5798850a360d0d79b5ab2ae42fda6e43f8cf3c8
Chris Lytsell <chrislytsell@chrislytsell-2.local>
2018-07-17 14:50:57 -0700

    asf

--- app.js
+++ app.js
@@ -1,34 +1,158 @@
 import express from 'express'
 import graphqlHTTP from 'express-graphql'
-import http from 'http'
+import { buildSchema, execute, subscribe } from 'graphql'
+import { find, filter } from 'lodash'
+import { createServer } from 'http'
 
-import { graphql, buildSchema } from 'graphql'
+// graphql.js JavaScript schema objects
+import {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLInputObjectType,
+  GraphQLEnumType,
+  GraphQLNonNull,
+  GraphQLList,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLFloat,
+  GraphQLID
+} from 'graphql'
 
-// Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
+// We'll serve on port 4000
+const PORT = 4000
 
-// The root provides a resolver function for each API endpoint
-var root = {
-  hello: () => {
-    return 'Hello world!';
-  },
-}
-
-// The canonical simplest graphQL server there ever was
+// init express app and http server
 var app = express()
-const server = http.createServer(app)
+const server = createServer(app)
+
+
+// Bring in our mock data
+import { agents, authors, posts } from "./data"
+
+// And the resolvers
+// import { resolvers } from './resolvers'
+
+const PostTypeEnum = new GraphQLEnumType({
+  name: 'PostTypeEnum',
+  description: 'Various types of content for a given post',
+  values: {
+    NEWS: {
+      value: 1,
+      description: 'News story',
+    },
+    SPORTS: {
+      value: 2,
+      description: 'A rare type of post',
+    },
+    OPINION: {
+      value: 3,
+      description: 'Everybody has one',
+    },
+  }
+})
+
+// Note because of circular reference, there is a "thunk" in this def
+const PostType = new GraphQLObjectType({
+  name: 'Post',
+  description: 'A single post by a single author',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(GraphQLID),
+          resolve: (root, args, context, info) => {
+            return root.id
+            }
+          },
+    title: { type: new GraphQLNonNull(GraphQLString),
+              resolve: (root, args, context, info) => {
+                return root.title
+                }
+              },
+    author: { type: new GraphQLNonNull(AuthorType),
+               resolve: (root, args, context, info) => {
+                 console.log('Root: ' + JSON.stringify(root))
+                 return root.author
+                }
+              },
+    articleType: { type: PostTypeEnum,
+                  // No resolver!!!  It's automatic!!
+                  }
+          })
+  })
+
+  const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    description: 'A single post by a single author',
+    fields: {
+      id: { type: new GraphQLNonNull(GraphQLID),
+            resolve: (root, args, context, info) => {
+              return root.id
+              }
+            },
+      firstName: { type: new GraphQLNonNull(GraphQLString),
+                   resolve: (root, args, context, info) => {
+                     return root.firstName
+                   }
+                },
+      middleInitial: { type: new GraphQLNonNull(GraphQLString),
+                       resolve: (root, args, context, info) => {
+                         return root.middleInitial
+                          }
+                      },
+      lastName: { type: new GraphQLNonNull(GraphQLString),
+                  resolve: (root, args, context, info) => {
+                   return root.lastName
+                  }
+                },
+      posts: { type: new GraphQLList(PostType),
+                resolve: (root, args, context, info) => {
+                  return filter(posts, { authorId: root.id })
+                    }
+            }
+          }
+        })
+
+const mySchema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Query',
+    description: 'Query authors and posts',
+      fields: {
+        posts: {
+          type: new GraphQLList(PostType),
+          args: {},
+          resolve: (root, args, context, info) => {
+            return posts // Looking for all posts
+          }
+        },
+        author: {
+          type: AuthorType,
+          args: { id: { type: GraphQLInt } },
+          resolve: (root, args, context, info) => {
+            return find(authors, { id: args.id })
+          }
+        },
+        post: {
+          type: PostType,
+          args: { id: { type: GraphQLInt } },
+          resolve: (root, args, context, info) => {
+            return find(posts, { id: args.id })
+          }
+        },
+        authors: {
+          type: new GraphQLList(AuthorType),
+          args: {},
+          resolve: (root, args, context, info) => {
+            return authors
+          }
+        }
+      }
+  })
+})
 
 app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
+  schema: mySchema,
   graphiql: true,
 }))
 
-server.listen(4000)
+server.listen(4000);
 console.log(
   'Express server listening on port %d in %s mode',
   server.address().port, app.settings.env
